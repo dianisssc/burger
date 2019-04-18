eb60d821a377bf38f874eb384da30698ea09e4aa
Dianis Carbajal <dianiscarbajal@gmail.com>
2019-04-18 16:40:04 -0400

    files

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,9 +1,30 @@
-
-
-$(".form").on("submit",function (event){
+// This will add the burger when the submit button is clicked.
+$(".form").on("submit", function (event) {
     event.preventDefault();
-    console.log("hi1")
+    var burgerHolder = {
+        burgerIn: $("#burgerText").val()
+    }
+    $.post("/api/burgers", burgerHolder)
+        .then(function () {
+            location.reload()
+        })
+});
 
+// This will move the burger to the "devoured" section when the "Devour" button is clicked
+$(".devourBtn").on("click", function () {
+    var id = $(this).val()
+
+    var devourObj = {
+        devoured: true
+    }
+
+    $.ajax({
+        method: "PUT",
+        url: "/api/burgers/" + id,
+        data: devourObj
+    }).then(function () {
+        location.reload()
+    })
 })
 
 
